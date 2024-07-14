9bdfd0d00fbc1152f3413dfce5c36a25783e9c3b
Abdirahmanelyas <abdirahman.elyas@student.moringaschool.com>
2024-07-14 19:36:44 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+
+// Given values
+let num1 = 31;
+let num2 = 2;
+
+// 1. Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62.
+let multiply = num1 * num2;
+
+// 2. Create a variable called random that will generate a random integer greater than 0.
+let random = Math.floor(Math.random() * 100) + 1;
+
+// Given values for remainder calculation
+let num3 = 14;
+let num4 = 5;
+
+// 3. Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+let mod = num3 % num4;
+
+// 4. Create a variable called max that finds the highest number in a set; the value returned should be 20.
+let numbers = [1, 2, 3, 20];
+let max = Math.max(...numbers);
